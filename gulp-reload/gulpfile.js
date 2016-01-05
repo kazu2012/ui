9f071e14b4e9722cf4052ac193651ce5cc5bdbab
mamoru2110 <mamoru310@yahoo.com>
2016-01-05 14:30:10 +0900

    Delete old comments.

--- gulp-reload/gulpfile.js
+++ gulp-reload/gulpfile.js
@@ -1,9 +1,6 @@
-// ライブラリの読み込み。
 var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
  
-// サーバ起動タスク。
-// src がベースディレクトリ。
 gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
@@ -13,15 +10,11 @@ gulp.task('browser-sync', function() {
     });
 });
  
-// ブラウザリロードタスク。
 gulp.task('bs-reload', function () {
     browserSync.reload();
 });
  
-// デフォルトタスク。
-// 事前にサーバ起動タスクを実行する。
-// src 配下の *.html, *.css ファイルを監視する。
-// ファイルが変更されたら、ブラウザリロードタスクを実行する。
+// src 配下の *.html, *.css ファイルが変更されたリロード。
 gulp.task('default', ['browser-sync'], function () {
     gulp.watch("src/*.html", ['bs-reload']);
     gulp.watch("src/*.css", ['bs-reload']);
